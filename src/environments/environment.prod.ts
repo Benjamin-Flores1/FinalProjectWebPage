e28d586aa89cc1c8c4d7fb352d2a9403cdb4634c
aguss1254 <agusmeneses0@gmail.com>
2022-11-29 11:26:32 -0300

    Login agregado

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB4RkLtjWoY1iTQfP6mQ58Goc3ISdSuYpI",
+    authDomain: "softtek-azure.firebaseapp.com",
+    projectId: "softtek-azure",
+    storageBucket: "softtek-azure.appspot.com",
+    messagingSenderId: "217260755016",
+    appId: "1:217260755016:web:eaf7d4b40c890263ab307d",
+    measurementId: "G-1QQXN9NPNS"
+  }
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB4RkLtjWoY1iTQfP6mQ58Goc3ISdSuYpI",
+    authDomain: "softtek-azure.firebaseapp.com",
+    projectId: "softtek-azure",
+    storageBucket: "softtek-azure.appspot.com",
+    messagingSenderId: "217260755016",
+    appId: "1:217260755016:web:eaf7d4b40c890263ab307d",
+    measurementId: "G-1QQXN9NPNS"
+  }
+};
+
+
